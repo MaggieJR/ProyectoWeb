0e35a4b393bd02e82859077822c9881067af5a70
Magaly <magalyjimeno@gmail.com>
2023-01-13 23:35:02 -0600

    Primer Comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+ {path: '', redirectTo: 'layout', pathMatch: 'full'},
+ {path: 'layout', component: LayoutComponent},
+ {path: 'home', component: HomeComponent},
+ {path: 'login', component:LoginComponent},
+ {path: 'registro', component:RegistroComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
